9ace37dde4bf41508c3651f238dd0976e6111068
furyjack <lakshaytaneja26@gmail.com>
2016-04-30 20:03:57 +0530

    problem 9
    easiest quetsion on earth knew the answer right away dint put /n after
    every answer got wrong answer when submitted wasted my time so pissed
    off

---
+++ packaging cupcakes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+ int no_cases;
+ long long int n;
+ cin>>no_cases;
+
+ for(long long int i=0;i<no_cases;i++)
+ {
+     cin>>n;
+     cout<<(n/2)+1<<endl;
+ }
+
+
+
+
+}
